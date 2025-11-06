22ff84c4646341b63469fce6f0d00eba888e4209
jericjan <cantilfrederick@gmail.com>
2025-11-06 21:24:34 +0800

    test: putting MidiPlayer in multiprocessing (slows down)

--- smd/midi.py
+++ smd/midi.py
@@ -1,4 +1,5 @@
 import ctypes
+import multiprocessing
 from pathlib import Path
 
 from smd.structs import MidiFiles
@@ -9,17 +10,33 @@ logger = logging.getLogger(__name__)
 
 class MidiPlayer:
     def __init__(self, dll: Path):
-        lib_path = str(dll.resolve())
-        try:
-            self.player_lib = ctypes.CDLL(lib_path)
-        except OSError as e:
-            raise ValueError(f"Error loading library: {e}")
+        self.lib_path = str(dll.resolve())
 
-        self.CIntArray16 = ctypes.c_int * 16
+
+        
         self.soundfont = str(MidiFiles.SOUNDFONT.value.resolve()).encode()
         self.midi = str(MidiFiles.MIDI.value.resolve()).encode()
+        self.work_queue: multiprocessing.Queue[str] = multiprocessing.Queue()
+        self.child = multiprocessing.Process(target=self.worker, args=(self.work_queue,))
+        self.child.start()
+
+    def worker(self, queue: "multiprocessing.Queue[str]"):
+        self.CIntArray16 = ctypes.c_int * 16
+        try:
+            self.player_lib = ctypes.CDLL(self.lib_path)
+        except OSError as e:
+            raise ValueError(f"Error loading library: {e}")        
+        while True:
+            work = queue.get()
+            if work == "start":
+                self._start()
+            elif work == "stop":
+                self._stop()
 
     def start(self):
+        self.work_queue.put("start")
+
+    def _start(self):
         # All channels on by default
         initial_states_py = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
@@ -70,9 +87,12 @@ class MidiPlayer:
     def set_channel(self, channel_num: int, state: int):
         self.player_lib.ToggleChannel(channel_num, state)
 
-    def set_range(self, start: int, end: int, state: int):
+    def _set_range(self, start: int, end: int, state: int):
         for x in range(start, end + 1):
             self.set_channel(x, state)
 
-    def stop(self):
+    def _stop(self):
         self.player_lib.StopPlayback()
+
+    def stop(self):
+        self.work_queue.put("stop")
