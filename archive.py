42e6cff06a7ab82fce426212e6aeaf0bb9b5d9af
jericjan <38642145+jericjan@users.noreply.github.com>
2025-12-31 12:13:31 +0800

    build: add linux support for archiving the build

--- archive.py
+++ archive.py
@@ -11,18 +11,32 @@ from smd.strings import LINUX_RELEASE_PREFIX, WINDOWS_RELEASE_PREFIX
 def archive():
     main_folder = Path.cwd() / "dist/main"
     prefix = 'unsupported'
+    file_type = ''
     if sys.platform == "win32":
         prefix = WINDOWS_RELEASE_PREFIX
+        file_type = ".zip"
     elif sys.platform == "linux":
         prefix = LINUX_RELEASE_PREFIX
-    zip_file = main_folder.parent / f"{prefix}_SMD_{VERSION}.zip"
+        file_type = ".tar.xz"
+    zip_file = main_folder.parent / f"{prefix}_SMD_{VERSION}{file_type}"
 
     if zip_file.exists():
         zip_file.unlink()
 
-    with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
-        files = list(main_folder.rglob("*"))
-        count = len(files)
-        for idx, file in enumerate(files):
-            print(f"{idx+1} / {count}")
-            zf.write(file, file.relative_to(main_folder))
+    if sys.platform == "win32":
+        with zipfile.ZipFile(
+            zip_file, "w", zipfile.ZIP_DEFLATED, compresslevel=9
+        ) as zf:
+            files = list(main_folder.rglob("*"))
+            count = len(files)
+            for idx, file in enumerate(files):
+                print(f"{idx+1} / {count}")
+                zf.write(file, file.relative_to(main_folder))
+    elif sys.platform == "linux":
+        tar = shutil.which("tar")
+        if not tar:
+            print("tar not found. can't archive build into tar.xz file.")
+        subprocess.run([tar, "-cJvf", str(zip_file.resolve()), "-C", main_folder, "."])
+
+if __name__ == "__main__":
+    archive()
